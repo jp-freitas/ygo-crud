7e1c87e7f2a490a5047e81c230a9150d50632ec7
João Pedro de Deus Freitas <tec.joao.freitas@gmail.com>
2024-08-19 21:06:36 +0000

    dev: Updated the title of the page and added icons in the social media login buttons.

--- ygo-crud-front/src/app/page.tsx
+++ ygo-crud-front/src/app/page.tsx
@@ -1,18 +1,19 @@
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+import { GoogleLogo, FacebookLogo, XLogo } from "@phosphor-icons/react/dist/ssr";
 
 export default function Home() {
   return (
-    <main className="flex flex-col w-1/2 space-y-2 p-8 justify-center border-2 border-slate-50 rounded-3xl">
+    <main className="flex flex-col w-1/4 space-y-2 p-8 justify-center border-2 border-slate-50 rounded-3xl">
       <h1 className="text-slate-50 text-3xl">Log In</h1>
       <p className="text-slate-50">Log in in our application to control your statistics.</p>
       <Input placeholder="e-mail" type="email" id="email" />
       <Input placeholder="senha" type="password" id="password" />
       <Button>logar</Button>
       <div className="flex w-full h-full space-x-2">
-        <Button className="bg-blue-700 w-full rounded p-3 text-slate-50 capitalize">log in with facebook</Button>
-        <Button className="bg-red-700 w-full rounded p-3 text-slate-50 capitalize">log in with google</Button>
-        <Button className="bg-cyan-700 w-full rounded p-3 text-slate-50 capitalize">log in with twitter</Button>
+        <Button className="bg-blue-700 w-full rounded p-3 text-slate-50 capitalize flex items-center justify-center"><FacebookLogo size={32} color="#ffffff" /></Button>
+        <Button className="bg-red-700 w-full rounded p-3 text-slate-50 capitalize flex items-center justify-center"><GoogleLogo size={32} color="#ffffff" /></Button>
+        <Button className="bg-cyan-700 w-full rounded p-3 text-slate-50 capitalize flex items-center justify-center"><XLogo size={32} color="#ffffff" /></Button>
       </div>
     </main>
   );
