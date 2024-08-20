5d2afd0ec95d2735e1f0fe87e04aa32d5b3b2c11
João Pedro de Deus Freitas <tec.joao.freitas@gmail.com>
2024-08-20 02:50:04 +0000

    dev: starting the home page of the application.

---
+++ ygo-crud-front/src/app/home/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <main className="min-h-screen w-screen p-0 text-slate-300 flex">
+            <aside className="w-1/4 min-h-screen bg-slate-600 p-2 flex flex-col items-start gap-4 mr-1">
+                <h1>Dashboard</h1>
+                <h1>LP Calculator</h1>
+                <h1>Statistic Profile</h1>
+            </aside>
+            <section className="w-3/4 min-h-screen bg-slate-50 p-2 flex ml-1">
+
+            </section>
+        </main>
+    )
+}
--- ygo-crud-front/src/app/page.tsx
+++ ygo-crud-front/src/app/page.tsx
@@ -1,15 +1,24 @@
+'use client' 
+
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
 import { GoogleLogo, FacebookLogo, XLogo } from "@phosphor-icons/react/dist/ssr";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  function handleNavigation() {
+    router.push('/home');
+  }
+
   return (
     <main className="flex flex-col w-1/4 space-y-2 p-8 justify-center border-2 border-slate-50 rounded-3xl">
       <h1 className="text-slate-50 text-3xl">Log In</h1>
       <p className="text-slate-50">Log in in our application to control your statistics.</p>
       <Input placeholder="e-mail" type="email" id="email" />
       <Input placeholder="senha" type="password" id="password" />
-      <Button>logar</Button>
+      <Button onClick={handleNavigation}>logar</Button>
       <div className="flex w-full h-full space-x-2">
         <Button className="bg-blue-700 w-full rounded p-3 text-slate-50 capitalize flex items-center justify-center"><FacebookLogo size={32} color="#ffffff" /></Button>
         <Button className="bg-red-700 w-full rounded p-3 text-slate-50 capitalize flex items-center justify-center"><GoogleLogo size={32} color="#ffffff" /></Button>
